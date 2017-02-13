c43c3596bacfae60ab39b4b9b23123f5401572bc
srisha-eeshan <srisha-eeshan@192.168.1.25>
2017-02-12 21:09:51 -0700

    Comit

--- src/main/java/com/arkiv/sports/referee/Application.java
+++ src/main/java/com/arkiv/sports/referee/Application.java
@@ -15,9 +15,9 @@ public class Application  extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(Application.class);
+//	}
 
 }
